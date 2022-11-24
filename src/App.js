40332762c16d8748ad69d2db32518268d7935fe1
pooja <pooja@>
2022-11-24 17:32:32 +0530

    complete button

--- src/App.js
+++ src/App.js
@@ -1,110 +1,139 @@
-import React,{useState, useEffect} from 'react'
-import { View } from './component/View';
+import React, { useState, useEffect } from "react";
+import { View } from "./component/View";
 
 // getting the values of local storage
-const getDatafromLS=()=>{
-  const data = localStorage.getItem('users');
-  if(data){
+const getDatafromLS = () => {
+  const data = localStorage.getItem("users");
+  if (data) {
     return JSON.parse(data);
+  } else {
+    return [];
   }
-  else{
-    return []
-  }
-}
+};
 
 export const App = () => {
-
   // main array of objects state || books state || books array of objects
-  const [users, setUsers]=useState(getDatafromLS());
+  const [users, setUsers] = useState(getDatafromLS());
   // input field states
-  const [id, setId]=useState('');
-  const [fname, setFname]=useState('');
-  const [lname, setLname]=useState('');
-  const [dob, setDob]=useState('');
+  const [id, setId] = useState("");
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [dob, setDob] = useState("");
 
   // form submit event
-  const handleAddBookSubmit=(e)=>{
+  const handleAddBookSubmit = (e) => {
     e.preventDefault();
     // creating an object
-    let user={
+    let user = {
       id,
       fname,
       lname,
-      dob
-    }
-    setUsers([...users,user]);
-  }
+      dob,
+    };
+    setUsers([...users, user]);
+  };
 
   // delete book from LS
-  const deleteUser=(id)=>{
-    const filteredBooks=users.filter((element,index)=>{
-      return element.id !== id
-    })
+  const deleteUser = (id) => {
+    const filteredBooks = users.filter((element, index) => {
+      return element.id !== id;
+    });
     setUsers(filteredBooks);
-  }
+  };
 
   // saving data to local storage
-  useEffect(()=>{
-    localStorage.setItem('users',JSON.stringify(users));
-  },[users])
+  useEffect(() => {
+    localStorage.setItem("users", JSON.stringify(users));
+  }, [users]);
 
   return (
-    <div className='wrapper'>
+    <div className="wrapper">
       <h1>UserList App</h1>
-    
-      <div className='main'>
 
-        <div className='form-container'>
-          <form autoComplete="off" className='form-group'
-          onSubmit={handleAddBookSubmit}>
+      <div className="main">
+        <div className="form-container">
+          <form
+            autoComplete="off"
+            className="form-group"
+            onSubmit={handleAddBookSubmit}
+          >
             <label>ID</label>
-            <input type="number" className='form-control' required
-            onChange={(e)=>setId(e.target.value)} value={id}></input>
+            <input
+              type="number"
+              className="form-control"
+              required
+              onChange={(e) => setId(e.target.value)}
+              value={id}
+            ></input>
             <br></br>
             <label>First Name</label>
-            <input type="text" className='form-control' required
-            onChange={(e)=>setFname(e.target.value)} value={fname}></input>
+            <input
+              type="text"
+              className="form-control"
+              required
+              onChange={(e) => setFname(e.target.value)}
+              value={fname}
+            ></input>
             <br></br>
             <label>Last Name</label>
-            <input type="text" className='form-control' required
-            onChange={(e)=>setLname(e.target.value)} value={lname}></input>
+            <input
+              type="text"
+              className="form-control"
+              required
+              onChange={(e) => setLname(e.target.value)}
+              value={lname}
+            ></input>
             <br></br>
             <label>DOB</label>
-            <input type="date" className='form-control' required
-            onChange={(e)=>setDob(e.target.value)} value={dob}></input>
+            <input
+              type="date"
+              className="form-control"
+              required
+              onChange={(e) => setDob(e.target.value)}
+              value={dob}
+            ></input>
             <br></br>
-            <button type="submit" className='btn btn-success btn-md'>
+            <button type="submit" className="btn btn-success btn-md">
               ADD
             </button>
           </form>
         </div>
 
-        <div className='view-container'>
-          {users.length>0&&<>
-            <div className='table-responsive'>
-              <table className='table'>
-                <thead>
-                  <tr>
-                    <th>ID</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>DOB</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <View users={users} deleteUser={deleteUser} />
-                </tbody>
-              </table>
-            </div>
-            <button className='btn btn-danger btn-md'
-            onClick={()=>setUsers([])}>Remove All</button>
-          </>}
+        <div className="view-container">
+          {users.length > 0 && (
+            <>
+              <div className="table-responsive">
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>First Name</th>
+                      <th>Last Name</th>
+                      <th>DOB</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <View
+                      users={users}
+                      deleteUser={deleteUser}
+                     
+                    />
+                  </tbody>
+                </table>
+              </div>
+              <button
+                className="btn btn-danger btn-md"
+                onClick={() => setUsers([])}
+              >
+                Remove All
+              </button>
+            </>
+          )}
           {users.length < 1 && <div>No users are added yet</div>}
         </div>
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/component/View.js
+++ src/component/View.js
@@ -1,19 +1,30 @@
-import React from 'react'
-import {Icon} from 'react-icons-kit'
-import {trash} from 'react-icons-kit/feather/trash'
+import React, { useState } from "react";
+import { Icon } from "react-icons-kit";
+import { trash } from "react-icons-kit/feather/trash";
 
-export const View = ({users,deleteUser}) => {
-    
-    return users.map(book=>(
-        
-        <tr key={book.id}>
-            <td>{book.id}</td>
-            <td>{book.fname}</td>
-            <td>{book.lname}</td>
-            <td className='delete-btn' onClick={()=>deleteUser(book.id)}>
-                <Icon icon={trash}/>
-            </td>           
-        </tr>            
-    
-))
-}
+export const View = ({ users, deleteUser }) => {
+  
+    const [line, setLine] = useState(false);
+
+    const completeHandle = (id) => {
+      console.log("Complete",id);
+      setLine(true);
+    };
+
+  return users.map((book) => (
+    <tr
+      key={book.id}
+      style={{textDecoration:line ? "line-through":"none"}}
+    >
+      <td>{book.id}</td>
+      <td>{book.fname}</td>
+      <td>{book.lname}</td>
+      <td className="delete-btn" onClick={() => deleteUser(book.id)}>
+        <Icon icon={trash} />
+      </td>
+      <td className="complete-btn" onClick={() => completeHandle(book.id)}>
+        Complete task
+      </td>
+    </tr>
+  ));
+};
